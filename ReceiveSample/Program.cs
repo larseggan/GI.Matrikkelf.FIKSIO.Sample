33ca311047f4767e25c0b37dda9a97d26aaae726
Arne Bakken <Arne.Bakken@norconsult.com>
2021-11-17 12:59:19 +0100

    Added recieve sample program

---
+++ ReceiveSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReceiveSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
